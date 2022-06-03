ef9b4379be65b82f365defc470176ae520d75d0d
Volodymyr Kyselevych <wilkarich@gmail.com>
2022-06-03 20:58:53 +0300

    Edited: fixed sider on the page

--- src/pages/MainLayout/MainLayout.tsx
+++ src/pages/MainLayout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import {Divider, Layout} from "antd";
+import {Layout} from "antd";
 import React from "react";
 import Navigation from "components/Navigation/Navigation";
 import {Outlet} from "react-router-dom";
@@ -6,13 +6,24 @@ import {Outlet} from "react-router-dom";
 function MainLayout() {
     return (
         <Layout style={{height: "100vh"}}>
-            <Layout.Sider className="sider" width={250}>
+            <Layout.Sider
+                className="sider"
+                width={250}
+                style={{
+                    overflow: 'auto',
+                    height: '100vh',
+                    position: 'fixed',
+                    left: 0,
+                    top: 0,
+                    bottom: 0,
+                }}
+            >
                 <div className="sider__logo">
                     <span>JS</span> Education
                 </div>
                 <Navigation/>
             </Layout.Sider>
-            <Layout.Content>
+            <Layout.Content style={{marginLeft: 250}}>
                 <div className="content">
                     <Outlet/>
                 </div>
